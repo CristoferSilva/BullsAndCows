5356f6ab6f4a77cb18c12ca5ae51215de94f0c26
Matheus Coelho Muniz dos Santos <macomusa14@gmail.com>
2022-12-02 23:20:47 -0300

    Finished about me screen.

--- app/src/main/java/com/cristoferSilva/bullandcows/fragments/AboutMatheusFragment.kt
+++ app/src/main/java/com/cristoferSilva/bullandcows/fragments/AboutMatheusFragment.kt
@@ -1,14 +1,16 @@
 package com.cristoferSilva.bullandcows.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cristoferSilva.bullandcows.MainActivity
 import com.cristoferSilva.bullandcows.R
 
 class AboutMatheusFragment : Fragment() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,7 +20,13 @@ class AboutMatheusFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val aboutFragment = inflater.inflate(R.layout.fragment_about_matheus, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_matheus, container, false)
+        val buttonLinkedin = aboutFragment.findViewById<View>(R.id.btn_linkedin_matheus)
+
+        buttonLinkedin.setOnClickListener(){
+            startActivity( Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/in/matheus-coelho005/")))
+        }
+        return aboutFragment;
     }
 }
